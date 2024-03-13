71d3eda84bb7ef8f15d1bbb5a818e96f870be765
chandrabhan <chandrabhansingh27d@gmail.com>
2024-03-13 21:46:25 +0530

    feat: Tailwind install and store setup

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,11 +1,43 @@
+import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      } catch (error) {
+        console.log("error in getting user info in :: App.jsx", error)
+      } finally {
+        setLoading(false);
+      }
+    })();
 
 
-  return (
-    <>
-    <h1>Blog App with Appwrite</h1>
-    </>
-  )
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap justify-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
